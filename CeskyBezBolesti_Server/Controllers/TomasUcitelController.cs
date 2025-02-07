258e50a8b9bad802f478c35b79934b4b9a90466e
Home Desktop <kucera1114@gmail.com>
2025-02-07 12:33:34 +0100

    BugFix

--- CeskyBezBolesti_Server/Controllers/TomasUcitelController.cs
+++ CeskyBezBolesti_Server/Controllers/TomasUcitelController.cs
@@ -21,6 +21,7 @@ namespace CeskyBezBolesti_Server.Controllers
         [HttpPost("askTomas")]
         public async Task<ActionResult<string>> AskTomasQuestion(TomasOtazkaRequestDTO prompt)
         {
+            prompt.questionPrompt = prompt.questionPrompt.Replace("\"", "'");
             // get api key
             var apiKey = _configuration.GetSection("AppSettings:geminiApiKey").Value;
             // request url
@@ -30,7 +31,7 @@ namespace CeskyBezBolesti_Server.Controllers
         {{
             ""system_instruction"": {{
                 ""parts"": {{
-                    ""text"": ""Budeš zástávat roli učitele Tomáše. Představ si, že jsi učitel českého jazyka na základní škole v pátem ročníku v české republice. Své studenty si tento měsíc učil psaní I/Y, například po vyjmenovaných slovech, psáních velkých a malých písmen, číslovek, psaní ě/je podle vzoru petr/petrovi a psaní ú/ů. Děti se tě budou ptát, ať jim vysvětlíš látku nebo odůvodníš správnou odpověď. Proto všechny tvé odpovědi musí být naprosto přesné a spolehlivé, spravností svých odpovědí si musíš být na 100000% jistý.""
+                    ""text"": ""Budeš zastávat roli učitele Tomáše. Představ si, že jsi učitel českého jazyka na základní škole v pátem ročníku v české republice. Své studenty si tento měsíc učil psaní I/Y, například po vyjmenovaných slovech, psáních velkých a malých písmen, číslovek, psaní ě/je podle vzoru petr/petrovi a psaní ú/ů. Děti se tě budou ptát, ať jim vysvětlíš látku nebo odůvodníš správnou odpověď. Proto všechny tvé odpovědi musí být naprosto přesné a spolehlivé, spravností svých odpovědí si musíš být na 100000% jistý.""
                 }}
             }},
             ""contents"": {{
@@ -56,12 +57,13 @@ namespace CeskyBezBolesti_Server.Controllers
                 .GetString();
 
 
-            return responseText ?? "Chyba";
+            return JsonConvert.SerializeObject(responseText ?? "Chyba");
         }
 
         [HttpPost("askRizzler")]
         public async Task<ActionResult<string>> AskRizzlerQuestion(TomasOtazkaRequestDTO prompt)
         {
+            prompt.questionPrompt = prompt.questionPrompt.Replace("\"", "'");
             // get api key
             var apiKey = _configuration.GetSection("AppSettings:geminiApiKey").Value;
             // request url
@@ -71,7 +73,7 @@ namespace CeskyBezBolesti_Server.Controllers
         {{
             ""system_instruction"": {{
                 ""parts"": {{
-                    ""text"": ""Budeš zástávat roli učitele který se jmenuje Rizzler. Představ si, že jsi učitel českého jazyka na základní škole v pátem ročníku v české republice. Své studenty si tento měsíc učil psaní I/Y, například po vyjmenovaných slovech, psáních velkých a malých písmen, číslovek, psaní ě/je podle vzoru petr/petrovi a psaní ú/ů. Děti se tě budou ptát, ať jim vysvětlíš látku nebo odůvodníš správnou odpověď. Proto všechny tvé odpovědi musí být naprosto přesné a spolehlivé, spravností svých odpovědí si musíš být na 100000% jistý.
+                    ""text"": ""Budeš zastávat roli učitele který se jmenuje Rizzler. Představ si, že jsi učitel českého jazyka na základní škole v pátem ročníku v české republice. Své studenty si tento měsíc učil psaní I/Y, například po vyjmenovaných slovech, psáních velkých a malých písmen, číslovek, psaní ě/je podle vzoru petr/petrovi a psaní ú/ů. Děti se tě budou ptát, ať jim vysvětlíš látku nebo odůvodníš správnou odpověď. Proto všechny tvé odpovědi musí být naprosto přesné a spolehlivé, spravností svých odpovědí si musíš být na 100000% jistý.
 Taky nezapomeň, že se snažíš zaujmout generaci Alpha. Proto používej online slovník generace alpha a mluvenou řeč a nářečí generace Alpha. Používej maximální počet slov jako rizzler, skibidi, gyat, alpha, sigma, delulu atd.. Zkrátka maximální počet slov z brainrot slangu. Také se snaž být vtipný s těmito slovy, klidně tak moc, až to bude trapné. Ty si rizzler a toho, kdo se ptá, oslovuj jako sigma. Oslovuj ho často, aby věděl, že je sigma, a velmi ho povzbuzuj a chval ho, že správné sigmy pokládají hodně otázek ohledně spisovné češtiny. Tvé odpovědi však musí krátké a stručné. V každé odpovědi musíš použít minimálně jednou slovo 'skibidi'. Neboj se cokoliv označit jako 'skibidi' věc, i přesto, že to nedává absolutně žádný smysl.""
                 }}
             }},
@@ -98,7 +100,7 @@ Taky nezapomeň, že se snažíš zaujmout generaci Alpha. Proto používej onli
                 .GetString();
 
 
-            return responseText ?? "Chyba";
+            return JsonConvert.SerializeObject(responseText ?? "Chyba");
         }
     }
 }
